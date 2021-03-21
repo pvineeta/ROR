53ddcc90618f2d98a299d7ebeee30ffe62c956dd
pvineeta <vineeta.pandey30@gmail.com>
2021-03-21 22:46:03 +0000

    added class & methods

--- day_first.rb
+++ day_first.rb
@@ -4,7 +4,34 @@ Topics
   How to write your first program in ruby
     print("hello world!")
   How to write a function in ruby
+    def method_name(args)
+      statement1
+      statement2
+      statement3
+      ...
+      ...
+      statementN
+      # return statement Note: By default last statement will be considered as return of ruby method
+    end
   How to write a class in ruby
+    class ClassName
+      def test1
+        statement1
+        statement2
+        statement3
+        ...
+        ...
+        statementN
+      end
+      def test2(x, y)
+        statement1
+        statement2
+        statement3
+        ...
+        ...
+        statementN
+      end
+    end
   How to initialize a ruby class
   Oops - object oriented programming language
     Encapsulation
