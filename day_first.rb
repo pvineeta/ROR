9ff13a829dfb0613b49b15d01b68ae32a3f607b5
pvineeta <vineeta.pandey30@gmail.com>
2021-03-21 22:15:45 +0000

    first session

---
+++ day_first.rb
@@ -0,0 +1,12 @@
+Topics
+  keywords
+  identifiers
+  How to write your first program in ruby
+  How to write a function in ruby
+  How to write a class in ruby
+  How to initialize a ruby class
+  Oops - object oriented programming language
+    Encapsulation
+    Polymorphism
+    Inheritance
+    Abstraction
