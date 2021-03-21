ba62d197aec12bbfff038ad7a8958678a2ef8c5c
pvineeta <vineeta.pandey30@gmail.com>
2021-03-21 22:26:47 +0000

    my first ruby prog

--- day_first.rb
+++ day_first.rb
@@ -1,7 +1,8 @@
 Topics
-  keywords
-  identifiers
+  keywords - reserve words
+  identifiers - class name, function name, varaible name etc ..
   How to write your first program in ruby
+    print("hello world!")
   How to write a function in ruby
   How to write a class in ruby
   How to initialize a ruby class
---
+++ my_first_ruby_prg.rb
@@ -0,0 +1,7 @@
+p("Hello world!")
+
+
+# there are three printing keywords in ruby which are as given below
+# print does not takes new line and return nil
+# p - takes a new line and return printing statement
+# puts - takes a new line and return nil
