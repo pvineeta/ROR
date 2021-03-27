2ce530d4392223a592ea897011d4b9b4fd6c3953
pvineeta <vineeta.pandey30@gmail.com>
2021-03-27 23:15:52 +0000

    Day3

---
+++ rails/day_3rd.rb
@@ -0,0 +1,7 @@
+Change rails default home page to our own html page
+to do so go to your routes and make the changes as given below
+
+root to: "controller_name#page_name"
+
+#How to install bootstrap 4.3 on a Rails 6 app using Webpack
+https://medium.com/@guilpejon/how-to-install-bootstrap-4-3-in-a-rails-6-app-using-webpack-9eae7a6e2832
