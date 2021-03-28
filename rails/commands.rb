5218c2caa028d8fc82480b87e07945346306d804
pvineeta <vineeta.pandey30@gmail.com>
2021-03-28 00:44:55 +0000

    new addition

---
+++ rails/commands.rb
@@ -0,0 +1,7 @@
+To create a new app: rails new app_name
+To install a new gem: bundle install
+To start your rails app/server: rails s or rails server
+To see all available routes: rails routes
+To create a controller: rails g/generate controller controller_name followed by pages (index edit show)
+To create an instance: @controller name & message (go to controller file, inside a particular function put)
+To view the messages:<%= @pagename %>
